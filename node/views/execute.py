223029d25d57584947482991b5c6a9366d2aecde
IlDezmond <desmond578@mail.ru>
2023-11-22 13:34:08 +0500

    <task-5>: fix association

--- node/views/execute.py
+++ node/views/execute.py
@@ -2,7 +2,7 @@ from uc_flow_nodes.schemas import NodeRunContext
 from uc_flow_nodes.views import execute
 from uc_flow_schemas.flow import RunState
 
-from node.views.execute_handlers import handle_object
+from node.views.execute_handlers import handle_association, handle_object
 
 
 class ExecuteView(execute.Execute):
@@ -10,14 +10,15 @@ class ExecuteView(execute.Execute):
         try:
             hub_object = json.node.data.properties['object']
             token = json.node.data.properties['token']
-            method = json.node.data.properties['method']
 
             if hub_object == 'contacts':
+                method = json.node.data.properties['method']
                 await handle_object(json, token, method, hub_object)
             elif hub_object == 'deals':
+                method = json.node.data.properties['method']
                 await handle_object(json, token, method, hub_object)
             elif hub_object == 'association':
-                await handle_association(json, token, method)
+                await handle_association(json, token)
 
             json.state = RunState.complete
         except Exception as e:
